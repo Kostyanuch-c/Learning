dfbc3347f14c4fb9f94ba868ca7c95bf9cd48214
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-03-05 19:39:37 +0500

    New files

---
+++ education/abstraction_with_data.py
@@ -0,0 +1,31 @@
+from math import gcd
+
+
+def make(numer, denom):
+    nod = gcd(numer, denom)
+    return f'{int(numer / nod)}/{int(denom / nod)}'
+
+
+def get_numer(rat):
+    return int(rat.split('/')[0])
+
+
+def get_denom(rat):
+    return int(rat.split('/')[1])
+
+
+def add(rat1, rat2):
+    x1, y1 = get_numer(rat1), get_denom(rat1)
+    x2, y2 = get_numer(rat2), get_denom(rat2)
+    return make(x1 * y2 + x2 * y1, y1 * y2)
+
+
+def sub(rat1, rat2):
+    x1, y1 = get_numer(rat1), get_denom(rat1)
+    x2, y2 = get_numer(rat2), get_denom(rat2)
+    return make(x1 * y2 - x2 * y1, y1 * y2)
+
+
+rat1 = make(3, 9)
+print(get_numer(rat1))
+print(get_denom(rat1))
