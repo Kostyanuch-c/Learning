826c4b7c566e010666903602365c87e5de2b1f6c
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-03-09 13:35:34 +0500

    Update files

--- education/abstraction_with_data.py
+++ education/abstraction_with_data.py
@@ -24,8 +24,3 @@ def sub(rat1, rat2):
     x1, y1 = get_numer(rat1), get_denom(rat1)
     x2, y2 = get_numer(rat2), get_denom(rat2)
     return make(x1 * y2 - x2 * y1, y1 * y2)
-
-
-rat1 = make(3, 9)
-print(get_numer(rat1))
-print(get_denom(rat1))
